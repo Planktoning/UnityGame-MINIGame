287eeebd86209e88dab1ee383668ba35e74dc29f
cdy2002 <cdy2002@foxmail.com>
2022-10-23 22:18:24 +0800

    fix scenetransition bug

--- Assets/Scripts/Transition/TeleportBack02.cs
+++ Assets/Scripts/Transition/TeleportBack02.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleportTo03 : Teleport
+public class TeleportBack02 : Teleport
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Switch();
-            other.transform.position = new Vector3(-25, 6f, 0);
+            other.transform.position = new Vector3(-27, 6.5f, 0);
         }
     }
 }
